22315172b1cd9e1cf515a3a36fab9f9c8f2ecfdd
1995eaton <1995eaton@gmail.com>
2014-06-27 21:50:44 -0400

    formatting

--- test.js
+++ test.js
@@ -1,18 +1,18 @@
-// jsonp
+// JSONP
 httpRequest({
   url: 'http://jsfiddle.net/echo/jsonp/?example=1',
   jsonp: true
 }, function(response) {
-  console.log(response);
-}); // Object -> { example: 1 }
+  console.log(response); // Object -> { example: 1 }
+});
 
-// json
+// JSON
 httpRequest({
   url: 'http://www.reddit.com/.json',
   json: true
 }, function(response) {
-  console.log(response);
-}); // Object -> { kind: "Listing", data: Object }
+  console.log(response); // Object -> { kind: "Listing", data: Object }
+});
 
 // Post data
 httpRequest({
@@ -21,10 +21,10 @@ httpRequest({
     letters: 'rcsamleb'
   }
 }, function(response) {
-  console.log(response);
-}); // clambers,scramble
+  console.log(response); // String -> 'clambers,scramble'
+});
 
 // default XHR
 httpRequest(document.URL, function(response) {
-  console.log(response);
+  console.log(response); // String -> document.innerHTML
 });
